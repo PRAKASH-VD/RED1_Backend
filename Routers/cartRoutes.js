fdda2c76b8f2d0cef743c7188b183774dae3471d
PRAKASH-VD <prakashdjp4@gmail.com>
2025-08-06 09:40:49 +0530

    Property overwrite error update

--- Routers/cartRoutes.js
+++ Routers/cartRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
-import {addToCart,removeFromCart,updateCartQuantity,viewCart} from "../Controllers/cartController.js";
-
-
+import {
+  addToCart,
+  removeFromCart,
+  updateCartQuantity,
+  viewCart,
+} from "../Controllers/cartController.js";
+import { authMiddleware } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
-router.post("/add",authMiddleware, addToCart);
+router.post("/add", authMiddleware, addToCart);
 router.delete("/remove/:propertyId", authMiddleware, removeFromCart);
 router.put("/update/:propertyId", authMiddleware, updateCartQuantity);
 router.get("/view", authMiddleware, viewCart);
-
-
-
 
 export default router;
